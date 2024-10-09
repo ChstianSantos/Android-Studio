07277214845cba7e0c81bf474bdcc2aa64d3b1a1
Christian S Sousa <78213318+ChstianSantos@users.noreply.github.com>
2024-10-09 17:19:43 -0300

    Update container.kt

--- container.kt
+++ container.kt
@@ -1,9 +1,47 @@
-<?xml version="1.0" encoding="utf-8"?>
-<shape xmlns:android="http://schemas.android.com/apk/res/android"
-    android:shape="rectangle">
 
-    <solid android:color="@color/white"/>
-    <corners android:radius="30dp"/>
+package com.example.imccalculator
 
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.example.form.R
 
-</shape>
+class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Referências aos componentes da interface
+        val editTextAltura = findViewById<EditText>(R.id.editTextAltura)
+        val editTextPeso = findViewById<EditText>(R.id.editTextPeso)
+        val buttonCalcular = findViewById<Button>(R.id.buttonCalcular)
+        val textViewResultado = findViewById<TextView>(R.id.textViewResultado)
+
+        // Ação do botão Calcular
+        buttonCalcular.setOnClickListener {
+            // Verifica se os campos não estão vazios
+            val alturaStr = editTextAltura.text.toString()
+            val pesoStr = editTextPeso.text.toString()
+
+            if (alturaStr.isNotEmpty() && pesoStr.isNotEmpty()) {
+                val altura = alturaStr.toDouble()
+                val peso = pesoStr.toDouble()
+
+                // Calcula o IMC
+                val imc = peso / (altura * altura)
+
+                // Exibe o resultado no TextView
+                textViewResultado.text = String.format("Seu IMC é: %.2f", imc)
+            } else {
+                // Mostra um alerta se os campos estiverem vazios
+                Toast.makeText(this, "Por favor, insira altura e peso!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
