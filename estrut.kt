4f8646d0983622fd692055af2ef8d2a9c0f5ab72
Christian S Sousa <78213318+ChstianSantos@users.noreply.github.com>
2024-10-09 17:18:51 -0300

    Update estrut.kt

--- estrut.kt
+++ estrut.kt
@@ -1,178 +1,63 @@
-<?xml version="1.0" encoding="utf-8"?>
-<androidx.constraintlayout.widget.ConstraintLayout xmlns:android="http://schemas.android.com/apk/res/android"
+<LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
     xmlns:app="http://schemas.android.com/apk/res-auto"
     xmlns:tools="http://schemas.android.com/tools"
-    android:id="@+id/main"
     android:layout_width="match_parent"
     android:layout_height="match_parent"
-    android:background="@drawable/bw"
-    tools:context=".MainActivity">
+    android:orientation="vertical"
+    android:padding="20dp">
 
-    <View
-        android:id="@+id/conteinerCadastro"
-        android:layout_width="376dp"
-        android:layout_height="699dp"
-        android:layout_marginHorizontal="20dp"
-        app:layout_constraintBottom_toBottomOf="parent"
-        app:layout_constraintEnd_toEndOf="parent"
-        app:layout_constraintStart_toStartOf="parent"
-        app:layout_constraintTop_toTopOf="parent" />
-
+    <!-- Campo para inserir a altura -->
     <TextView
-        android:id="@+id/txtTitle"
-        android:layout_width="wrap_content"
+        android:id="@+id/textView"
+        android:layout_width="match_parent"
         android:layout_height="wrap_content"
-        android:text="Cadastro"
-        android:textSize="25sp"
-        android:textColor="@color/blue"
-        android:textStyle="bold"
-        android:layout_marginTop="30dp"
-        app:layout_constraintTop_toTopOf="@id/conteinerCadastro"
-        app:layout_constraintStart_toStartOf="@id/conteinerCadastro"
-        app:layout_constraintEnd_toEndOf="@id/conteinerCadastro"/>
-
-    <TextView
-        android:id="@+id/txtDesc"
-        android:layout_width="wrap_content"
-        android:layout_height="wrap_content"
-        android:layout_marginTop="4dp"
-        android:text="Faça o cadastro!"
-        android:textColor="@color/blue"
+        android:text="Calculando PESINHO do angrybids"
+        android:textAlignment="center"
+        android:textColor="#E20E0E"
         android:textSize="18sp"
-        android:textStyle="bold"
-        app:layout_constraintEnd_toEndOf="@id/conteinerCadastro"
-        app:layout_constraintHorizontal_bias="0.495"
-        app:layout_constraintStart_toStartOf="@id/conteinerCadastro"
-        app:layout_constraintTop_toBottomOf="@id/txtTitle" />
+        android:textStyle="bold|italic"
+        android:typeface="serif"
+        tools:ignore="HardcodedText" />
 
     <EditText
-        android:id="@+id/editTextName"
-        android:layout_width="196dp"
-        android:layout_height="43dp"
-        android:ems="10"
-        android:inputType="text"
-        android:hint="Nome"
-        android:textColorHint="@color/blue"
-        app:layout_constraintTop_toBottomOf="@id/txtDesc"
-        app:layout_constraintStart_toStartOf="@id/conteinerCadastro"
-        app:layout_constraintEnd_toEndOf="@id/conteinerCadastro"
-        android:layout_marginTop="16dp"/>
+        android:id="@+id/editTextAltura"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:hint="Digite sua altura (em metros)"
+        android:inputType="numberDecimal"
+        android:layout_marginTop="10dp"
+        app:layout_constraintTop_toTopOf="@id/textView"
+        tools:ignore="HardcodedText" />
 
     <EditText
-        android:id="@+id/editTextAge"
-        android:layout_width="196dp"
-        android:layout_height="43dp"
-        android:ems="10"
-        android:inputType="number"
-        android:hint="Idade"
-        android:textColorHint="@color/blue"
-        app:layout_constraintTop_toBottomOf="@id/editTextName"
-        app:layout_constraintStart_toStartOf="@id/conteinerCadastro"
-        app:layout_constraintEnd_toEndOf="@id/conteinerCadastro"
-        android:layout_marginTop="8dp"/>
+        android:id="@+id/editTextPeso"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:hint="Digite seu peso (em kg)"
+        android:inputType="numberDecimal"
+        android:layout_marginTop="10dp"
+        app:layout_constraintTop_toTopOf="@id/editTextAltura"
+        tools:ignore="HardcodedText" />
 
-    <EditText
-        android:id="@+id/editTextGender"
-        android:layout_width="196dp"
-        android:layout_height="43dp"
-        android:ems="10"
-        android:inputType="text"
-        android:hint="Sexo"
-        android:textColorHint="@color/blue"
-        app:layout_constraintTop_toBottomOf="@id/editTextAge"
-        app:layout_constraintStart_toStartOf="@id/conteinerCadastro"
-        app:layout_constraintEnd_toEndOf="@id/conteinerCadastro"
-        android:layout_marginTop="8dp"/>
+    <TextView
+        android:id="@+id/textViewResultado"
+        android:layout_width="210sp"
+        android:layout_height="39dp"
+        android:layout_marginStart="67dp"
+        android:layout_marginTop="15dp"
+        android:text="Seu IMC aparecerá aqui"
+        android:textSize="18sp"
+        tools:ignore="HardcodedText" />
 
-    <EditText
-        android:id="@+id/editTextNationality"
-        android:layout_width="196dp"
-        android:layout_height="43dp"
-        android:ems="10"
-        android:inputType="text"
-        android:hint="Nacionalidade"
-        android:textColorHint="@color/blue"
-        app:layout_constraintTop_toBottomOf="@id/editTextGender"
-        app:layout_constraintStart_toStartOf="@id/conteinerCadastro"
-        app:layout_constraintEnd_toEndOf="@id/conteinerCadastro"
-        android:layout_marginTop="8dp"/>
+    <Button
+        android:id="@+id/buttonCalcular"
+        android:layout_width="356dp"
+        android:layout_height="68dp"
+        android:backgroundTint="#E70C0C"
+        android:text="Calcular IMC"
+        android:textColor="#F8F4F4"
+        android:textSize="20sp"
+        app:rippleColor="#FFFFFF"
+        tools:ignore="HardcodedText" />
 
-    <EditText
-        android:id="@+id/editTextStreet"
-        android:layout_width="196dp"
-        android:layout_height="43dp"
-        android:ems="10"
-        android:inputType="text"
-        android:hint="Rua"
-        android:textColorHint="@color/blue"
-        app:layout_constraintTop_toBottomOf="@id/editTextNationality"
-        app:layout_constraintStart_toStartOf="@id/conteinerCadastro"
-        app:layout_constraintEnd_toEndOf="@id/conteinerCadastro"
-        android:layout_marginTop="8dp"/>
-
-    <EditText
-        android:id="@+id/editTextNeighborhood"
-        android:layout_width="196dp"
-        android:layout_height="43dp"
-        android:ems="10"
-        android:inputType="text"
-        android:hint="Bairro"
-        android:textColorHint="@color/blue"
-        app:layout_constraintTop_toBottomOf="@id/editTextStreet"
-        app:layout_constraintStart_toStartOf="@id/conteinerCadastro"
-        app:layout_constraintEnd_toEndOf="@id/conteinerCadastro"
-        android:layout_marginTop="8dp"/>
-
-    <EditText
-        android:id="@+id/editTextState"
-        android:layout_width="196dp"
-        android:layout_height="43dp"
-        android:ems="10"
-        android:inputType="text"
-        android:hint="Estado"
-        android:textColorHint="@color/blue"
-        app:layout_constraintTop_toBottomOf="@id/editTextNeighborhood"
-        app:layout_constraintStart_toStartOf="@id/conteinerCadastro"
-        app:layout_constraintEnd_toEndOf="@id/conteinerCadastro"
-        android:layout_marginTop="8dp"/>
-
-    <EditText
-        android:id="@+id/editTextNumber"
-        android:layout_width="196dp"
-        android:layout_height="43dp"
-        android:ems="10"
-        android:inputType="number"
-        android:hint="Número"
-        android:textColorHint="@color/blue"
-        app:layout_constraintTop_toBottomOf="@id/editTextState"
-        app:layout_constraintStart_toStartOf="@id/conteinerCadastro"
-        app:layout_constraintEnd_toEndOf="@id/conteinerCadastro"
-        android:layout_marginTop="8dp"/>
-
-    <EditText
-        android:id="@+id/editTextComplement"
-        android:layout_width="196dp"
-        android:layout_height="43dp"
-        android:ems="10"
-        android:inputType="text"
-        android:hint="Complemento"
-        android:textColorHint="@color/blue"
-        app:layout_constraintTop_toBottomOf="@id/editTextNumber"
-        app:layout_constraintStart_toStartOf="@id/conteinerCadastro"
-        app:layout_constraintEnd_toEndOf="@id/conteinerCadastro"
-        android:layout_marginTop="8dp"/>
-
-    <EditText
-        android:id="@+id/editTextShoes"
-        android:layout_width="196dp"
-        android:layout_height="43dp"
-        android:ems="10"
-        android:inputType="text"
-        android:hint="Chinelo ou Tênis"
-        android:textColorHint="@color/blue"
-        app:layout_constraintTop_toBottomOf="@id/editTextComplement"
-        app:layout_constraintStart_toStartOf="@id/conteinerCadastro"
-        app:layout_constraintEnd_toEndOf="@id/conteinerCadastro"
-        android:layout_marginTop="8dp"/>
-
-</androidx.constraintlayout.widget.ConstraintLayout>
+</LinearLayout>
